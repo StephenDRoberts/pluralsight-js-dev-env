c0d6dea481c467535c11f88091e0b938356a7973
Steve <roberts.stephen.david@gmail.com>
2018-09-05 22:30:43 +0100

    up to Ch9: Linting

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,8 +2,9 @@ import express from 'express';
 import path from 'path';
 import open from 'open';
 import webpack from 'webpack';
-import config from '../webpack.congif.dev';
+import config from '../webpack.config.dev';
 
+/*eslint-disable no-console*/
 
 const port = 3000;
 const app = express();
@@ -18,7 +19,7 @@ app.get('/',function(req,res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err,){
+app.listen(port, function(err){
   if(err){
     console.log(err);
   } else{
